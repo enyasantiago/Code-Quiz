0bce19883a7c47d5214c2200843b612f09abd7ce
Your NameEnyaSantiagogit config --global user.namegit config --global user.name Enya <enyasantiago@me.com>
2021-01-16 21:19:12 -0500

    updated Read Me file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Code-Quiz
-Code Quiz
+Code Quiz with timer which decrements the timer when user answers incorrectly. Questions are multiple choice. User submits initials to be added to the high scores list. User has the option to go back and play again or clear all high scores. App made with use of Visual Studio Code and Chrome.
--- questions.js
+++ questions.js
@@ -1,27 +1,27 @@
 var questions = [
 {
   title: "q1",
-  choices: ["answer1 ", "answer2", "answer3", "answer4", "answer5"],
+  choices: ["answer1", "answer2", "answer3", "answer4", "answer5"],
   answer: "answer1",
 },
 {
   title: "q2",
-  choices: ["answer1 ", "answer2", "answer3", "answer4", "answer5"],
+  choices: ["answer1", "answer2", "answer3", "answer4", "answer5"],
   answer: "answer2",
 },
 {
   title: "q3",
-  choices: ["answer1 ", "answer2", "answer3", "answer4", "answer5"],
+  choices: ["answer1", "answer2", "answer3", "answer4", "answer5"],
   answer: "answer3",
 },
 {
   title: "q4",
-  choices: ["answer1 ", "answer2", "answer3", "answer4", "answer5"],
+  choices: ["answer1", "answer2", "answer3", "answer4", "answer5"],
   answer: "answer4",
 },
 {
   title: "q5",
-  choices: ["answer1 ", "answer2", "answer3", "answer4", "answer5"],
+  choices: ["answer1", "answer2", "answer3", "answer4", "answer5"],
   answer: "answer5",
 },
 ];
